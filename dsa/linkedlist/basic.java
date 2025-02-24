9a1589c637e55e23c3b82bb4cd8bc62d34a0a31e
Akash Srivastava <55024565+akash-srivastava-pvt@users.noreply.github.com>
2025-02-24 18:58:03 +0530

    Create basic.java

---
+++ dsa/linkedlist/basic.java
@@ -0,0 +1,49 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    // Insert at the end
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Print the linked list
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        
+        list.printList(); // Output: 1 -> 2 -> 3 -> 4 -> null
+    }
+}
