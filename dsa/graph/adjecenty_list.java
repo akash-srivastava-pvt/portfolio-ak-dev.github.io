5aa897f7ef1e29b9b9bfddd4407dc5fc0a6866f9
Akash Srivastava <55024565+akash-srivastava-pvt@users.noreply.github.com>
2025-02-24 18:37:05 +0530

    Create adjecenty_list.java

---
+++ dsa/graph/adjecenty_list.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Graph {
+    private int vertices;
+    private LinkedList<Integer>[] adjList;
+
+    @SuppressWarnings("unchecked")
+    Graph(int v) {
+        vertices = v;
+        adjList = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int src, int dest) {
+        adjList[src].add(dest);
+        adjList[dest].add(src); // Remove for directed graph
+    }
+
+    void printGraph() {
+        for (int i = 0; i < vertices; i++) {
+            System.out.print(i + " -> ");
+            for (int node : adjList[i]) {
+                System.out.print(node + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(5);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.printGraph();
+    }
+}
