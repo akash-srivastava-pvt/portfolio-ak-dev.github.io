d020ac1bef7dd15eadc7a93e104e40fbf4cc512b
Akash Srivastava <55024565+akash-srivastava-pvt@users.noreply.github.com>
2025-02-24 18:58:22 +0530

    Create reverseLL.java

---
+++ dsa/linkedlist/reverseLL.java
@@ -0,0 +1,10 @@
+public void reverse() {
+    Node prev = null, current = head, next = null;
+    while (current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+}
