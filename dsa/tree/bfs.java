1b63d51fe8f0395f4284ef19c63e5b61105db2dd
Akash Srivastava <55024565+akash-srivastava-pvt@users.noreply.github.com>
2025-02-24 18:43:10 +0530

    Create bfs.java

---
+++ dsa/tree/bfs.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class TreeTraversalBFS {
+    void levelOrder(TreeNode root) {
+        if (root == null) return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.print(node.val + " ");
+            if (node.left != null) queue.offer(node.left);
+            if (node.right != null) queue.offer(node.right);
+        }
+    }
+}
