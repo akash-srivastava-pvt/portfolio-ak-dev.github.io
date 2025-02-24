7b4d4e2a9cb96eb4f5e8ba154d29d404db627b49
Akash Srivastava <55024565+akash-srivastava-pvt@users.noreply.github.com>
2025-02-24 18:58:43 +0530

    Create middleNode.java

---
+++ dsa/linkedlist/middleNode.java
@@ -0,0 +1,8 @@
+public Node findMiddle() {
+    Node slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow; // Middle node
+}
